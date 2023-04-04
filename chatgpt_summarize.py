2d60ac024a220f51302a8db96ef36b9b8f217573
Zhixin <zhi.xingxing21@gmail.com>
2023-04-03 20:43:32 -0400

    fix: suffix for default file name

--- chatgpt_summarize.py
+++ chatgpt_summarize.py
@@ -11,6 +11,12 @@ SUMMARY_PROMPT = "Summarize the following text:"
 MODEL_NAME = "gpt-3.5-turbo"
 MODEL_TOKEN_LIMIT = 4096
 
+# Suffix for every format defined
+suffix_dict = {
+    'excel': 'xlsx',
+    'csv': 'csv'
+}
+
 def write_to_file(data, file, format):
     if format == 'excel':
         data.to_excel(file, index = None)
@@ -49,5 +55,5 @@ if __name__ == '__main__':
             conversation.append({"role": "assistant", "content": summary})
             data_list.append({'page': page.number, 'text': text, 'summary': summary})
     data = pd.DataFrame(data_list)
-    output_file = args.output_file if args.output_file else f"summary.{args.format}"
+    output_file = args.output_file if args.output_file else f"summary.{suffix_dict[args.format]}"
     write_to_file(data, output_file, args.format)
